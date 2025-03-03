bf95e304f084a08859c7669309e5bf30d80a9e24
Zackgald <zgald@asu.edu>
2025-03-02 23:55:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import time
 import my_oled
-state = 0;
-while True: 
-  pass
-  time.sleep(1);
-  state = state+1;
-  if (state = 0) {
-  my_oled.print_text("test", 0,0)
-  }
-  if (state =1) {
-     my_oled.print_text("something else", 3,3)
-  }
+
+state = 0
+while True:
+    time.sleep(1)
+    state = state + 1
+    if state == 0:
+        my_oled.print_text("test", 0, 0)
+    elif state == 1:
+        my_oled.print_text("something else", 3, 3)
+    elif state == 2:
+        my_oled.fill(0)
+        my_oled.oled.line(0, 0, 10, 10, 1)
+        my_oled.show()
+    elif state == 3:
+        my_oled.fill(0)
+        my_oled.graphics.fill_rect(0, 0, 10, 10, 1)
+        my_oled.show()
+    if state > 3:
+        state = 0  # Reset state after 3
